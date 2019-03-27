d71a82c82c7f784fe8a8715742b24120ec5c841f
Zachary Skalko <zac_skalko@cargill.com>
2019-03-27 08:54:36 -0500

    Hodor

---
+++ index.js
@@ -0,0 +1,23 @@
+(function() {
+  "use strict";
+  function hodorify(subjectString) {
+    var rgx = /([a-z])\w+/gi;
+    return subjectString.replace(rgx, "hodor");
+  }
+
+  if (typeof module !== "undefined" && module.exports) {
+    hodorify.default = hodorify;
+    module.exports = hodorify;
+  } else if (
+    typeof define === "function" &&
+    typeof define.amd === "object" &&
+    define.amd
+  ) {
+    // register as 'classnames', consistent with npm package name
+    define("hodorify", [], function() {
+      return hodorify;
+    });
+  } else {
+    window.hodorify = hodorify;
+  }
+})();
